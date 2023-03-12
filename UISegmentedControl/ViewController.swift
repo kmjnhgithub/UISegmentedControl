c445418742f82df41b73c5f145e4f14990188756
@mike <mike@innotech.me>
2023-03-12 17:04:56 +0800

    SegmentedControl

--- UISegmentedControl/ViewController.swift
+++ UISegmentedControl/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cardImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func suitDidChange(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            cardImageView.image = UIImage(named: "car")!
+        case 1:
+            cardImageView.image = UIImage(named: "motorcycle")!
+        case 2:
+            cardImageView.image = UIImage(named: "shoes")!
+        default:
+            cardImageView.image = UIImage(named: "car")!
+        }
+    }
 }
 
